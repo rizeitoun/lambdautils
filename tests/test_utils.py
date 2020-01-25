b803d9a0f09acf41ef8adfaa5d0522636de1bd47
Ramsey Zeitoun <r.i.zeitoun@gmail.com>
2020-01-25 12:04:00 -0500

    Updated test path.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,14 @@
 import unittest
 import util
 import json
+import os
 
+example_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'post_case_git_delete.json')
 
 class EncodingDecoding(unittest.TestCase):
 
     def test_dictionary_encode(self):
-        with open('data/post_case_git_delete.json') as f:
+        with open(example_file) as f:
             test_data = json.load(f)
         body = util.dictionary_encode(test_data['body'])
         self.assertEqual(str, type(body))
@@ -15,7 +17,7 @@ class EncodingDecoding(unittest.TestCase):
         self.assertEqual(len(body), 5982)
 
     def test_validate_hash(self):
-        with open('data/post_case_git_delete.json') as f:
+        with open(example_file) as f:
             test_data = json.load(f)
 
         body = util.dictionary_encode(test_data['body'])
